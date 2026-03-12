c7aa8f63d7ee7791f681b4fc951b09e5ca567dc1
AndyElo <andreajuarezayala40@gmail.com>
2026-03-12 13:54:33 -0600

    Update acript.js

--- acript.js
+++ acript.js
@@ -1,15 +1,1 @@
-document.getElementById("loginForm").addEventListener("submit", function(e){
 
-e.preventDefault();
-
-const user = document.getElementById("user").value;
-const pass = document.getElementById("pass").value;
-
-console.log("Credenciales capturadas:");
-console.log("Usuario:", user);
-console.log("Contraseña:", pass);
-
-document.getElementById("status").textContent =
-"Error de autenticación. Intente nuevamente.";
-
-});
